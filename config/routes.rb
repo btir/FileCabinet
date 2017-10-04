b3b78a1c69aa533866e96814c4835870ad3c4eb4
Hamid Bidokhti <hamidbidokhti@live.com>
2017-10-04 21:25:38 +0330

    Add docs view and define Actions And sure! everything works ;)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resource :docs
+  resources :docs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
